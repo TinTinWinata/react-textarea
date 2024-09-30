e36ec8d1d806972ca9521fb92905b09c3c0c6e03
TinTinWinata <tintin6892@gmail.com>
2024-09-30 20:47:30 +0700

    feat: ref forward

--- src/TextArea.tsx
+++ src/TextArea.tsx
@@ -1,57 +1,68 @@
-import React, { TextareaHTMLAttributes, useEffect, useRef } from "react";
+import React, {
+  TextareaHTMLAttributes,
+  useEffect,
+  useRef,
+  forwardRef,
+  useImperativeHandle,
+} from "react";
 
 export interface TextAreaProps
-  extends Omit<TextareaHTMLAttributes<HTMLTextAreaElement>, "onChange"> {
+  extends TextareaHTMLAttributes<HTMLTextAreaElement> {
   placeholder?: string;
   className?: string;
   rows?: number;
-  onChange?: (text: string) => void;
+  onChangeText?: (text: string) => void;
   initialSize?: number;
 }
 
-export default function TextArea({
-  initialSize = 100,
-  onChange,
-  style,
-  ...props
-}: TextAreaProps) {
-  const ref = useRef<HTMLTextAreaElement>(null);
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ initialSize = 100, onChangeText, onChange, style, ...props }, ref) => {
+    const innerRef = useRef<HTMLTextAreaElement>(null);
 
-  const baseStyle: React.CSSProperties = {
-    width: "100%",
-    resize: "none",
-    padding: "12px",
-    border: "1.5px solid #ccc",
-    borderRadius: "5px",
-    minHeight: `${initialSize}px`,
-    fontFamily: "inherit",
-    fontSize: "1rem",
-    lineHeight: "1.5",
-  };
+    const baseStyle: React.CSSProperties = {
+      width: "100%",
+      resize: "none",
+      padding: "12px",
+      border: "1.5px solid #ccc",
+      borderRadius: "5px",
+      minHeight: `${initialSize}px`,
+      fontFamily: "inherit",
+      fontSize: "1rem",
+      lineHeight: "1.5",
+    };
 
-  function checkSize() {
-    const element = ref.current;
-    if (element) {
-      element.style.height = "inherit";
-      element.style.height = `${Math.max(element.scrollHeight, initialSize)}px`;
+    function checkSize() {
+      const element = innerRef.current;
+      if (element) {
+        element.style.height = "inherit";
+        element.style.height = `${Math.max(
+          element.scrollHeight,
+          initialSize
+        )}px`;
+      }
     }
-  }
 
-  useEffect(() => {
-    if (ref && ref.current) {
-      checkSize();
-    }
-  }, [ref, ref.current]);
-
-  return (
-    <textarea
-      {...props}
-      ref={ref}
-      style={{ ...baseStyle, ...style }}
-      onChange={(e) => {
+    useEffect(() => {
+      if (innerRef.current) {
         checkSize();
-        onChange && onChange(e.currentTarget.value);
-      }}
-    />
-  );
-}
+      }
+    }, [innerRef.current]);
+
+    useImperativeHandle(ref, () => innerRef.current!);
+
+    return (
+      <textarea
+        {...props}
+        ref={innerRef}
+        style={{ ...baseStyle, ...style }}
+        onChange={(e) => {
+          checkSize();
+          onChange && onChange(e);
+          onChangeText && onChangeText(e.currentTarget.value);
+        }}
+      />
+    );
+  }
+);
+
+export default TextArea;
